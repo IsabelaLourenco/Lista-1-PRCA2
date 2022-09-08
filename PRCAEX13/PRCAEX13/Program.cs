99c36661b615f804c7a74a52afe43eb0534c91a1
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-09-08 12:35:09 -0300

    Add files via upload

---
+++ PRCAEX13/PRCAEX13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRCAEX13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo tri;
+            tri = new Triangulo();
+            Console.Write("Digite a hipotenusa: ");
+            tri.setA(int.Parse(Console.ReadLine()));
+            Console.Write("Digite o cateto: ");
+            tri.setB(int.Parse(Console.ReadLine()));
+            Console.Write("Digite o cateto: ");
+            tri.setC(int.Parse(Console.ReadLine()));
+            tri.hipotenusa();
+            if (tri.getA() == tri.getS())
+            {
+                Console.WriteLine("é um triangulo retangulo");
+            }
+            if (tri.getA() != tri.getS())
+            {
+                Console.WriteLine(" não é um triangulo retangulo");
+            }
+
+
+
+        }
+    }
+}
---
+++ PRCAEX13/PRCAEX13/Triangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRCAEX13
+{
+    class Triangulo
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int soma;
+        public void setA(int n)
+        {
+            a = n;
+        }
+        public void setB(int n)
+        {
+            b = n;
+        }
+        public void setC(int n)
+        {
+            c = n;
+        }
+        public int getA()
+        {
+            return a;
+        }
+        public int getB()
+        {
+            return b;
+        }
+        public int getC()
+        {
+            return c;
+        }
+        public int getS()
+        {
+            return soma;
+        }
+        public void hipotenusa()
+        {
+            soma = (b*b)+(c*c);
+            
+        }
+        
+    }
+}
